4f491a69631f901cb1d193236f02ced1b9ad4216
djm0321 <54867834+djm0321@users.noreply.github.com>
2022-04-27 04:36:12 -0400

    seeing if github can deal with this many thumbnails

--- mosaic.m
+++ mosaic.m
@@ -1,4 +1,6 @@
-% PhotoData = readtable('naturePhotos.csv', 'Delimiter','comma');
+% Create Empty Data table for image info. If images not downloaded,
+% download them. If they have been downloaded, open up .csv containing
+% image information.
 PhotoData = table();
 if exist('NaturePhotos', 'dir')
     PhotoData = readtable('naturePhotos.csv', 'Delimiter','comma');
@@ -6,36 +8,34 @@ else
     PhotoData = downloadImages();
 end
 
-
-I = imread("IMG_2609.jpg");
-mosaic1 = makeMosaic(I, PhotoData);
-imshow(mosaic1)
-
+% Iterate through all images in ./srcImages and make mosaic for each
+sourceImages = dir("srcImages");
+sourceImages = sourceImages(3:end, :);
+for i = 1:size(sourceImages, 1)
+    imSrc = ['./srcImages/' sourceImages(i, :).name]
+    I = imread(imSrc);
+    m = makeMosaic(I, PhotoData);
+    figure; imshow(m);
+end
 
 
 function finishedMosaic = makeMosaic(I, PhotoData)
-[x, y, z] = size(I);
-a = round(sqrt((x * y)/2500 ))
-newX = floor(x/a)
+[y, x, z] = size(I);
+a = round(sqrt((y * x)/2500 ))
 newY = floor(y/a)
-newX * newY;
+newX = floor(x/a)
+newY * newX;
 imageList = [""];
-for i =1:newY
-    for j = 1:newX
-        j + (i-1) * newX
-        %imageList(end+1)
+for i =1:newX
+    for j = 1:newY
+        j + (i-1) * newY
         [I, id] = getRandomPhoto(I, i, j, a, PhotoData);
         PhotoData((PhotoData.Var1 == id), :).USAGE = PhotoData((PhotoData.Var1 == id), :).USAGE + 1;
 
     end
 end
-%imageList = imageList(2:end);
-%disp(imageList);
-%fMontage = montage(imageList, 'Size', [newX newY]);
-finishedMosaic = I(1:1:a*newX, 1:a*newY, :);
+finishedMosaic = I(1:1:a*newY, 1:a*newX, :);
 end
-%montageImage = imread(fMontage.cData);
-%imshow(montageImage)
 
 
 function [mosaicPhoto, id] = getRandomPhoto(I, paneX, paneY, a, PhotoData)
@@ -44,13 +44,6 @@ function [mosaicPhoto, id] = getRandomPhoto(I, paneX, paneY, a, PhotoData)
     aR = mean(focusIm(:, :, 1), 'all');
     aG = mean(focusIm(:, :, 2), 'all');
     aB = mean(focusIm(:, :, 3), 'all');
-%     for row = 1:size(PhotoData, 1)
-%         if (PhotoData(row, :).USAGE) ~= -1
-%             PhotoData(row, :).DIST = abs(PhotoData(row, :).R - aR) + abs(PhotoData(row, :).G - aG) + abs(PhotoData(row, :).B - aB);
-%         else
-%            PhotoData(row, :).DIST = realmax;
-%         end
-%     end
     for distance = 105:-10:5
         testTable = PhotoData(abs(PhotoData.R - aR) < distance & abs(PhotoData.G - aG) < distance & abs(PhotoData.B - aB) < distance, :);
         if size(testTable, 1) < 20
@@ -59,7 +52,6 @@ function [mosaicPhoto, id] = getRandomPhoto(I, paneX, paneY, a, PhotoData)
         end
     end
     size(testTable, 1);
-    % x = ceil(rand * size(testTable, 1));
     testTable = sortrows(testTable, 'USAGE', 'ascend');
     temp = testTable(1, :).SRC;
     asd = testTable(1, :).Var1;
@@ -71,31 +63,6 @@ function [mosaicPhoto, id] = getRandomPhoto(I, paneX, paneY, a, PhotoData)
     I(1 + a * (paneY - 1): 1 + a * (paneY - 1) + (a - 1), 1 + a * (paneX - 1): 1 + a * (paneX - 1) + (a - 1), :) = I(1 + a * (paneY - 1): 1 + a * (paneY - 1) + (a - 1), 1 + a * (paneX - 1): 1 + a * (paneX - 1) + (a - 1), :) .* .3 + tester .* .7;
     mosaicPhoto = I;
     id = testTable(1, :).Var1;
-    %mosaicPhoto = temp{1};
-    % newTable = sortrows(PhotoData, 'DIST', 'ascend');
-    % testTable = newTable(1:10, :);
-%     size(testTable);
-%     minSSD = realmax;
-%     rPic = "";
-%     for i = 1:size(testTable, 1)
-%         tName = testTable(i, :).SRC;
-%         fImSize = size(focusIm);
-%         t = imread(tName{1});
-%         t = imresize(t, 50/a);
-%         size(t);
-%         rDiff = focusIm(:, :, 1) - t(:, :, 1);
-%         gDiff = focusIm(:, :, 2) - t(:, :, 2);
-%         bDiff = focusIm(:, :, 3) - t(:, :, 3);
-%         rSSD = sum(rDiff(:).^2);
-%         gSSD = sum(gDiff(:).^2);
-%         bSSD = sum(bDiff(:).^2);
-%         totalSSD = rSSD + gSSD + bSSD;
-%         if minSSD > totalSSD
-%             minSSD = totalSSD;
-%             rPic = tName{1};
-%         end
-%     end
-%     mosaicPhoto = rPic;
 end
 
 function tileTable = downloadImages()
@@ -111,7 +78,6 @@ function tileTable = downloadImages()
     targetSize = [50, 50];
     mkdir NaturePhotos
     for i = 1:size(PhotoData, 1)
-        % imgName = strcat('./MosaicStuff/',num2str(PhotoData.Var1(i)), '.jpg')
         imgName = strcat('./NaturePhotos/',num2str(PhotoData.Var1(i)), '.jpg')
         temp = PhotoData.URL(i);
         PhotoData.SRC(i) = {imgName};
@@ -140,4 +106,10 @@ function tileTable = downloadImages()
     PhotoData(toDelete, :) = [];
     writetable(PhotoData, 'naturePhotos.csv');
     tileTable = PhotoData;
+end
+
+function newData = GitHubURL(PhotoData)
+    for i = 1:size(PhotoData, 1)
+        
+    end
 end
